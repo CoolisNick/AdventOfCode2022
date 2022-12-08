fbde8af61cd5417e222e04b658ad8625d07fdbbd
Nick Morearty <nick@morearty.org>
2022-12-08 14:02:21 -0800

    Redo as a class

--- advent3/advent3.mjs
+++ advent3/advent3.mjs
@@ -1,58 +1,68 @@
 import fs from "fs";
 import readline from "readline";
 
-async function read(parseLine) {
-    return new Promise((resolve) => {
+class Run {
+    packs = [];
+    letters = [];
+    part;
+
+    constructor(options) {
+        this.part = options.part;
+    }
+
+    parseLinePt1(line) {
+        const pack1 = line.slice(0, line.length / 2);
+        const pack2 = line.slice(line.length / 2)
+        this.letters.push(this.findCommon(pack1, pack2));
+    }
+
+    findCommon(string1, string2, string3) {
+        for (const item of string1) {
+            if (string2.indexOf(item) !== -1 &&
+                (!string3 || string3.indexOf(item) !== -1)) {
+                return item;
+            }
+        }
+    }
+
+    sumLetters() {
+        let sum = 0;
+        for (const item of this.letters) {
+            const priority = item.charCodeAt(0);
+            if (priority > 96) {
+                sum += priority - 96;
+            } else {
+                sum += priority - 38;
+            }
+        }
+        console.log(`Part ${this.part} Sum: ${sum}`);
+    }
+
+    parseLinePt2(line) {
+        this.packs.push(line);
+        if (this.packs.length === 3) {
+            this.letters.push(this.findCommon(this.packs[0], this.packs[1], this.packs[2]))
+            this.packs = [];
+
+        }
+    }
+    
+    read() {
         const readStream = fs.createReadStream("input.txt", "utf-8");
-        const rl = readline.createInterface({input: readStream});
+        const rl = readline.createInterface({ input: readStream });
         rl.on("error", (error) => console.log(error.message));
-        rl.on("line", parseLine);
-        rl.on("close", () => {
-            sumLetters();
-            resolve();
+        rl.on("line", (line) => {
+            if (this.part === 1) {
+                this.parseLinePt1(line);
+            } else {
+                this.parseLinePt2(line);
+            }
         });
-    });
-}
-
-function parseLinePt1(line) {
-    const pack1 = line.slice(0, line.length / 2);
-    const pack2 = line.slice(line.length / 2)
-    letters.push(findCommon(pack1, pack2));
-}
-
-function findCommon(string1, string2, string3) {
-    for (const item of string1) {
-        if (string2.indexOf(item) !== -1 &&
-            (!string3 || string3.indexOf(item) !== -1)) {
-            return item;
-        }
+        rl.on("close", () => {
+            this.sumLetters();
+        });
     }
 }
 
-function sumLetters() {
-    let sum = 0;
-    for (const item of letters) {
-        const priority = item.charCodeAt(0);
-        if (priority > 96) {
-            sum += priority - 96;
-        } else {
-            sum += priority - 38;
-        }
-    }
-    console.log(`Sum: ${sum}`);
-}
-
-function parseLinePt2(line) {
-    packs.push(line);
-    if (packs.length === 3) {
-        letters.push(findCommon(packs[0], packs[1], packs[2]))
-        packs = [];
-
-    }
-}
-
-let packs = [];
-let letters = [];
-await read(parseLinePt1);
-letters = [];
-await read(parseLinePt2);
+new Run({ part: 1}).read();
+new Run({ part: 2}).read();
